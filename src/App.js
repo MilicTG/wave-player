fe11713cdae2547096b109b749e392a4634d1357
MilicTG <milic.tg@gmail.com>
2021-02-05 16:02:15 +0100

    Finished video 21

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   //audio ref
   const audioRef = useRef(null);
@@ -24,10 +25,17 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
+
+    //calculate percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+
     setSongInfo({
       ...songInfo,
       currentTime: current,
       duration: duration,
+      animationPercentage: animation,
     });
   };
 
